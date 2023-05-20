68a5c14bb8b2e30eba1cb8eb7f092d672ade7edb
supeRosyad <rosyadandro@gmail.com>
2023-05-20 19:48:40 +0700

    initioal commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const routes = require('./routes.js')
+const Hapi = require('@hapi/hapi');
+
+
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: "localhost",
+        routes: {
+            cors: {
+                origin: ['*']
+            }
+        }
+    })
+
+    server.route(routes);
+
+    await server.start();
+    console.log(`server is running on ${server.info.uri}`);
+}
+
+
+
+init();
